b1c8cab6c9735518cba5788d21eddaedc06a0e9e
Fei <feihong@hotmail.de>
2024-02-22 17:34:34 +0100

    email registery

--- front-end/libs/alphaweb/feat/auth/src/lib/register/register.component.ts
+++ front-end/libs/alphaweb/feat/auth/src/lib/register/register.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInput} from '@angular/material/input';
+import { MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'front-end-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInput, ReactiveFormsModule, MatCard, MatCardTitle, MatCardContent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+  registerForm: FormGroup;
+  // getErrorMessage() {
+  //   if (this.email.hasError('required')) {
+  //     return 'You must enter a value';
+  //   }
+  //
+  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  // }
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      //firstName: new FormControl(),
+      //lastName: new FormControl(),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      // password: new FormControl('', [Validators.required]),
+      // passwordConfirm: new FormControl('', [Validators.required]),
+    })
+  }
+
+  OnRegister() {
+    console.log('register button clicked', this.registerForm)
+  }
+}
